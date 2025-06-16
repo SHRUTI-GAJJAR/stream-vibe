df41ed6cc2687fe3aa9f913417b3436fa2027544
SHRUTI-GAJJAR <shrutiujeniya6@gmail.com>
2025-06-16 12:53:12 +0530

    Middleware

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
 const User = require('./../models/user');
+const authMiddleware = require('./../middlewares/authMiddleware')
 const router = express.Router();
 
 require("dotenv").config();
