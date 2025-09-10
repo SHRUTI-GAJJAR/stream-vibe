84d5ab7324a9d4795debe54d65dbf71dccae5515
SHRUTI-GAJJAR <shrutiujeniya6@gmail.com>
2025-09-10 18:12:49 +0530

    -----

--- routes/auth.js
+++ routes/auth.js
@@ -154,7 +154,7 @@ router.post("/forgot-password", async (req, res) => {
 
     // 5. Send mail
     await transporter.sendMail({
-      from: `"Stream Vibe Security" <${process.env.EMAIL_USER}>`,
+      from: `"Stream-Vibe-Security" <${process.env.EMAIL_USER}>`,
       to: user.email,
       subject: "Password Reset OTP",
       text: `Your OTP is ${otp}. It is valid for 15 minutes.`,
